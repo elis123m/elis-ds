70adcf8d0e69957879cb495e974d253b7dc07828
elis123m <164895538+elis123m@users.noreply.github.com>
2025-04-25 11:43:24 -0300

    Create pyton.py

---
+++ pyton.py
@@ -0,0 +1,19 @@
+class Filme:
+  delf __init__(self, nome, ano, duracao) :
+  self.nome = nome
+  self.ano = ano 
+self.duracao = duracao
+
+
+class Serie:
+  def __init__(self, nome, ano, temporadas):
+    self.nome = nome
+    self.ano = ano
+    self.temporadas = temporadas 
+
+a bela a fera = Filme('a bela e a fera', 1991, 1)
+print (a bela e a fera.nome)
+
+phineas e ferb = serie ('phineas e ferb', 2007, 4) 
+
+print(f'nome: {phineas e ferb.nome} - ano: {phineas e ferb.ano} - temporadas: {phineas e ferb.temporadas}')
